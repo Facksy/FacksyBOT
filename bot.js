efe739d75138b9d1e189bad93fc378770818d33a
Facksy <45769049+Facksy@users.noreply.github.com>
2020-06-24 20:38:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.author.id == '239554589072949249') {  //239554589072949249  701806342981025853
-    	message.reply('tg');
-        //message.channels.get('711204646860881940').send(message.content);
+    	message.reply('tgg');
+        message.channels.get('711204646860881940').send(message.content);
         message.delete();
     }
 });
