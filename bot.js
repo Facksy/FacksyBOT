58bfb8b6e6d02a200a208d7b1c861096fc406dd8
Facksy <45769049+Facksy@users.noreply.github.com>
2020-06-26 14:21:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,14 @@ client.on('message', message => {
     
     if (message.author.id != client.user.id){
         message.delete();
-        if(message.author.id == '701806342981025853' && Math.floor(Math.random() * Math.floor(5) == 1)){
+        let rand = Math.floor(Math.random() * Math.floor(2);
+        if(message.author.id == '701806342981025853' && rand == 1)){
            message.channel.send("DallasTHX0512 a dit: je suis ton fidèle esclave");
         }
-       else 
+       else {
            message.channel.send(message.author.username + " a dit: " + message.content);
-       // client.channels.get('711204646860881940').send(message.content);
+       }
+       client.channels.get('711204646860881940').send(message.content + " " + rand);
     }
     
 });
