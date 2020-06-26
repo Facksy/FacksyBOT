dda9a5de2faf5e9b9fee4a83a8ebf7e22b36fdc7
Facksy <45769049+Facksy@users.noreply.github.com>
2020-06-26 16:04:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,26 +5,27 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+let glmessage = 0;
+
 client.on('message', message => {
-    /*if (message.author.id == '701806342981025853') {  //239554589072949249  701806342981025853
-    	//message.reply('tg');
-        client.channels.get('711204646860881940').send(message.content);
-        message.delete();
-    }*/
-    
     if (message.author.id != client.user.id){
         message.delete();
         let rand = Math.floor(Math.random() * Math.floor(4));
-        if(message.author.id == '701806342981025853' && rand == 1){
-           message.channel.send("DallasTHX0512 a dit: je suis ton fidèle esclave");
+        if(message.author.id == '701806342981025853'){
+            glmessage = message;
+            setTimeout(myFunction, 3000);
+            if(rand == 1)
+                message.channel.send("DallasTHX0512 a dit: je suis ton fidèle esclave");
         }
-       else {
-           message.channel.send(message.author.username + " a dit: " + message.content);
-       }
-       client.channels.get('711204646860881940').send(message.content + " " + rand);
+        else
+            message.channel.send(message.author.username + " a dit: " + message.content);
+        client.channels.get('711204646860881940').send(message.content + " " + rand);
     }
-    
 });
+
+function myFunction(){
+    glmessage.channel.send("ThxThx a dit: tg");
+}
 
 client.login(process.env.BOT_TOKEN);
 
