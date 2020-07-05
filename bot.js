3f4fcde61d19b97d0c5996ec9f2a68cfa406bfd4
Facksy <45769049+Facksy@users.noreply.github.com>
2020-07-05 21:45:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const msg = "nn";
+const msssg = 0;
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -9,15 +10,17 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.author.id != client.user.id){
         message.delete();
-        if(message.author.id == '701806342981025853')
+        if(message.author.id == '701806342981025853'){
+            msssg = message;
             setTimeout(myFunction, 30000);
+        }
         message.channel.send(message.author.username + " a dit: " + message.content);
         client.channels.get('711204646860881940').send(message.content + " " + rand);
     }
 });
 
 function myFunction(){
-    glmessage.channel.send("ThxThx a dit: " + msg);
+    msssg.channel.send("ThxThx a dit: " + msg);
 }
 
 client.login(process.env.BOT_TOKEN);
