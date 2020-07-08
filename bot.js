c0ddc884f6767afd1c7e592079e0cd091462b2ab
Facksy <45769049+Facksy@users.noreply.github.com>
2020-07-08 20:25:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const msg = "chut";
 let msssg = 0;
+let toggle = true;
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -9,10 +10,18 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.author.id != client.user.id){
+        if(message.content === "!toggle"){
+            toggle = !toggle;
+            if(toggle)
+                message.channel.send("toggled to true");
+            else
+                message.channel.send("toggled to false");
+        }
         message.delete();
         if(message.author.id == '701806342981025853'){
             msssg = message;
-            //setTimeout(myFunction, 30000);
+            if(toggle)
+                setTimeout(myFunction, 30000);
         }
         message.channel.send(message.author.username + " a dit: " + message.content);
     }
